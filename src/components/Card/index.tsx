38137898a0365b744219faa32099f1644df894b3
Matheus Henrique <matheus.181137@gmail.com>
2023-04-11 20:15:55 -0300

    feat: allow card to receive a custom style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,8 +2,9 @@ import styles from './styles.module.css'
 
 type Props = {
 	children: React.ReactNode
+	customStyle?: string
 }
 
-export const Card: React.FC<Props> = ({ children }) => {
-	return <div className={styles.card}>{children}</div>
+export const Card: React.FC<Props> = ({ children, customStyle }) => {
+	return <div className={`${styles.card} ${customStyle}`}>{children}</div>
 }
